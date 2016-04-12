f16007195beb7a150cbf995703e3f9e42f11d221
fenglei <fenglei1@staff.weibo.com>
2016-04-12 14:52:09 +0800

    提交 iplook 查询模块

--- models/region_ip.go
+++ models/region_ip.go
@@ -13,13 +13,23 @@ func NewRegionIP(domain string, ip uint32) *RegionIP {
 
 	region := new(RegionIP)
 
-	v1, err := redis.Strings(db.Cli().Do( "zrangebyscore", "region_ip", ip, "+inf", "LIMIT", "0", "1" ))
+	// 查询 [Domain]IP库关联ID
+	v1, err := redis.Strings(db.Cli().Do( "zrangebyscore", domain+"_ip", ip, "+inf", "LIMIT", "0", "1" ))
+	if err != nil  {
+		fmt.Println( domain , "NewRegionIP err" )
+	}
+	if( len(v1) > 0 ){
+		region.IP = ip
+		region.ID = v1[0][ 0 : len(v1[0]) - 2 ]
+		return region
+	}
+
+ 	// 默认IP库关联ID
+	v1, err = redis.Strings(db.Cli().Do( "zrangebyscore", "region_ip", ip, "+inf", "LIMIT", "0", "1" ))
 	if err != nil || len(v1) == 0 {
 		fmt.Println( "NewRegionIP err" )
 	}
-
 	region.IP = ip
 	region.ID = v1[0][ 0 : len(v1[0]) - 2 ]
-
 	return region
 }
