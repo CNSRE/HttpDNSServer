a2712ae855d6330997d0c8bff3d2e1faae935cac
fenglei <fenglei1@staff.weibo.com>
2016-04-12 14:53:56 +0800

    修改 字段名

--- controllers/dns.go
+++ controllers/dns.go
@@ -26,8 +26,8 @@ func (c *DnsController) Get() {
 	longIp := util.IpStr2Long(ip)
 	regIp := models.NewRegionIP(domain ,longIp)
 	domainDns := models.NewDomainDNS( domain , regIp.ID )
-	ipinfo := models.NewIpInfo(regIp.ID)
-	data := models.NewdataModel( domain , ip , ipinfo.Isp , domainDns.A )
+	ipInfo := models.NewIpInfo(regIp.ID)
+	data := models.NewdataModel( domain , ip , ipInfo.Isp , domainDns.A )
 
 	c.Data["json"] = &data
 	c.ServeJSON()
